5aa2b9e1552787a6a672ce206750569ec6e636a4
Rob Stenson <rob.stenson@gmail.com>
2015-05-13 10:29:02 -0400

    better geo code

--- src/road/geo.clj
+++ src/road/geo.clj
@@ -6,45 +6,6 @@
             [clojure.pprint :refer [pprint]]
             [clj-xpath.core :as xp]))
 
-(comment "http://ogre.adc4gis.com/")
-(def ^:private roadtrip-map-url (string/trim (slurp "map-id")))
-
-(def ^:private kml (slurp "doc.kml"))
-
-(def ^:private colors->types
-  {"DB4436" "Indeterminate"
-   "0BA9CC" "Market"
-   "4186F0" "Restaurant"
-   "9FC3FF" "Restaurant"
-   "3F5BA9" "Bar"
-   "777777" "Museum"
-   "795046" "Museum"
-   "F4EB37" "POI"
-   "7C3592" "Coffee"
-   "A61B4A" "Coffee"
-   "62AF44" "Shopping"
-   "009D57" "Liquor Store"
-   "F4B400" "Hotel"
-   "FAD199" "Hotel"
-   "F8971B" "Hotel"
-   "CDDC39" "Venue"})
-
-(def ^:private types->markers
-  {"Indeterminate" ["#EEE"]
-   "Restaurant" ["#4186F0" "fast-food"]
-   "Bar" ["#3F5BA9" "bar"]})
-
-(defn- placemark->place [p]
-  (let [icon-url (xp/$x:text "./styleUrl" p)
-        color (second (re-matches #".*-([A-F0-9]{6})(-.*)?" icon-url))]
-    {:name (xp/$x:text "./name" p)
-     :color color
-     :icon (get colors->types color "N/A")
-     :coords (map #(Float/parseFloat %)
-                  (string/split
-                    (xp/$x:text "./Point/coordinates" p)
-                    #","))}))
-
 (defn ?assoc
   "Same as assoc, but skip the assoc if v is nil"
   [m & kvs]
@@ -54,23 +15,51 @@
        (map vec)
        (into m)))
 
-(defn- place->geojson-feature [{:keys [name color icon coords]}]
-  (let [[marker-color marker-symbol] (get types->markers icon ["#CCC"])
-        props {:title name :marker-size "medium"}]
+(def ^:private kml (slurp "doc.kml"))
+
+(defn- mark
+  ([name from-colors]
+    (mark name from-colors nil))
+  ([name from-colors icon]
+   {:name name
+    :from-colors (into #{} from-colors)
+    :color (str "#" (first from-colors))
+    :icon icon}))
+
+(def ^:private markers
+  [(mark "Indeterminate" ["DB4436"])
+   (mark "Market" ["0BA9CC"])
+   (mark "Restaurant" ["4186F0" "9FC3FF"] "fast-food")
+   (mark "Bar" ["3F5BA9"] "bar")
+   (mark "Museum" ["777777" "795046"] "museum")
+   (mark "POI" ["F4EB37"])
+   (mark "Coffee" ["7C3592" "A61B4A"])
+   (mark "Shopping" ["62AF44" "009D57"])
+   (mark "Hotel" ["F4B400" "FAD199" "F8971B"])
+   (mark "Venue" ["CDDC39"])])
+
+(defn- placemark->feature [p]
+  (let [name (xp/$x:text "./name" p)
+        icon-url (xp/$x:text "./styleUrl" p)
+        color (second (re-matches #".*-([A-F0-9]{6})(-.*)?" icon-url))
+        coords (map #(Float/parseFloat %)
+                    (string/split
+                      (xp/$x:text "./Point/coordinates" p)
+                      #","))
+        marker (first (filter #(contains? (:from-colors %) color) markers))]
     {:type "Feature"
      :geometry {:type "Point", :coordinates coords}
-     :properties (?assoc props
-                         :marker-color marker-color
-                         :marker-symbol marker-symbol)}))
+     :properties (?assoc {:title name
+                          :marker-size "medium"
+                          :marker-color (:color marker)}
+                         :marker-symbol (:icon marker))}))
 
 (defn to-geojson []
   (->> (xp/$x "//Folder" kml)
        (filter #(= "Restaurants" (xp/$x:text "./name" %)))
        (first)
        (xp/$x "./Placemark")
-       (map placemark->place)
-       ;(group-by :icon)
-       (map place->geojson-feature)
+       (map placemark->feature)
        (assoc {:type "FeatureCollection"} :features)
        (json/write-str)
        (spit "site/assets/js/features.json")
